3bc26c6ed2ca5dc04185c081c187e571712a15e7
SayCV <sssxyc@163.com>
2013-03-19 23:19:04 +0800

    update pkgs checksum: make.mk

--- src/make.mk
+++ src/make.mk
@@ -3,7 +3,7 @@
 
 PKG             := make
 $(PKG)_IGNORE   :=
-$(PKG)_CHECKSUM := 562471cbcb0dd0fa42a76665acf0dbb68479b78a
+$(PKG)_CHECKSUM := 92d1b87a30d1c9482e52fb4a68e8a355e7946331
 $(PKG)_SUBDIR   := make-$($(PKG)_VERSION)
 $(PKG)_FILE     := make-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := $(PKG_GNU)/make/$($(PKG)_FILE)
